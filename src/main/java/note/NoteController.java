929c80e59665eb654b65796fa9129756b0170048
Paul Williams <paul@paulswilliams.me.uk>
2016-10-01 13:13:45 +0100

    simple web app

---
+++ src/main/java/note/NoteController.java
@@ -0,0 +1,14 @@
+package note;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class NoteController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
